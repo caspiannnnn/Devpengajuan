0d1a633bba6ea10696e94f4ec8965ec0a2d2f7c5
caspiannnnn <ariviandyriesty@gmail.com>
2024-12-13 11:03:46 +0700

    Update

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -4,7 +4,7 @@
       {{-- Anything you want --}}
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2021 <a href="#">Creator</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2024 <a href="#">Universitas Merdeka Malang</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
--- resources/views/template/left-sidebar.blade.php
+++ resources/views/template/left-sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="{{asset('admin-lte-3/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Tugas Akhir</span>
+      <span class="brand-text font-weight-light">Sistem Informasi TA</span>
     </a>
 
     <!-- Sidebar -->
